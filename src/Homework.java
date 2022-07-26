0bdab0fe53e0f8e795e91608984316d6981adc9d
osamax01 <shkukanios@gmail.com>
2022-07-26 12:26:28 +0300

    add Point class

--- src/Homework.java
+++ src/Homework.java
@@ -1,5 +1,31 @@
 import stanford.karel.SuperKarel;
 
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
 public class Homework extends SuperKarel {
     public void run() {
         move();
